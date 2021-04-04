f8e90531465c4bf472a2280d897edd04355a63e1
BK927 <dead4beees927@gmail.com>
2021-04-05 01:33:12 +0900

    refactored css, removed word clouds, removed file content, added chatting statistics function, added counting phote video emoji youtube link function

--- js/main.js
+++ js/main.js
@@ -12,34 +12,40 @@ function readSingleFile(e) {
   const initFile = function (e) {
     const scriptData = createScriptObj(e.target.result);
     const fileContent = document.querySelector("#file-content");
-    fileContent.textContent = scriptData.getScript();
     hideHeader();
     setArticlesVisibility(true);
     displayHash(scriptData.getMd5Hash());
     displayPeriod(scriptData.getBeginDate(), scriptData.getEndDate());
+    displayChattingStatistics(scriptData.getBeginDate(), scriptData.getEndDate(), scriptData.numberOfLines)
+    displayMiscellaneous(
+      scriptData.getPhotoFrequency(),
+      scriptData.getVideoFrequency(),
+      scriptData.getEmojiFrequency(),
+      scriptData.getYoutubeFrequency()
+    );
     displayNameArticle(scriptData.getNameFrequency());
+    displayTypingRanking(scriptData.getTypingRanking());
     displayDayArticle(scriptData.getDayFrequency());
     displayHourArticle(scriptData.getHourFrequency());
-    //drawTextCloud(scriptData.getWordFrequency());
   };
 
   const hideHeader = function () {
-    const header = document.querySelector('header');
-    header.style.height = '0vh'; 
-    header.style.visibility = 'hidden';
-    header.style.margin = '0';
-  }
+    const header = document.querySelector("header");
+    header.style.height = "0vh";
+    header.style.visibility = "hidden";
+    header.style.margin = "0";
+  };
 
-  const clearArticles = function() {
+  const clearArticles = function () {
     const hash = document.querySelector("#hash-code");
-    const period = document.querySelector('#period');
-    const olist = document.querySelectorAll('article > ol');
-    hash.textContent = '';
-    period.textContent = '';
-    olist.forEach(element => {
-      element.innerHTML = '';
+    const period = document.querySelector("#period");
+    const olist = document.querySelectorAll("article > ol");
+    hash.textContent = "";
+    period.textContent = "";
+    olist.forEach((element) => {
+      element.innerHTML = "";
     });
-  }
+  };
 
   const setArticlesVisibility = function (flag) {
     const articles = document.querySelectorAll(".container > article");
@@ -55,30 +61,75 @@ function readSingleFile(e) {
     node.textContent = hash;
   };
 
-  const displayPeriod = function (beginDate, endDate) {
-    const node = document.querySelector('#period');
-    const convertDate = function (date) {
-      const year = String(date.getFullYear()) +'년';
-      const month = String(date.getMonth() + 1) + '월';
-      const day = String(date.getDate()) + '일';
+  const displayChattingStatistics = function(beginDate, endDate, numberOfLine){
+    const gapTime = endDate.getTime() - beginDate.getTime();
+    const gapDay = Math.floor(gapTime / (1000 * 60 * 60 * 24));
+    const parentNode = document.querySelector('#chatting-statistics');
 
-      return year + ' ' + month + ' ' + day;
+    {
+      const divider = document.createElement("div");
+      const title = document.createElement("h4");
+      const content = document.createElement("h5");
+      title.textContent = '전체 채팅 수';
+      content.textContent = String(numberOfLine);
+      divider.append(title);
+      divider.append(content);
+      parentNode.append(divider);
+    }
+
+    {
+      const divider = document.createElement("div");
+      const title = document.createElement("h4");
+      const content = document.createElement("h5");
+      title.textContent = '하루 평균 채팅 수';
+      content.textContent = String(Math.floor(numberOfLine/gapDay));
+      divider.append(title);
+      divider.append(content);
+      parentNode.append(divider);
+    }
+    
+  };
+
+  const displayMiscellaneous = function (numberOfPhoto, numberOfVideo, numberOfEmoji, numberOfYoutube) {
+    const parentNode = document.querySelector("#number-of-miscellaneous");
+    const arr = [
+      ["사진 수", numberOfPhoto],
+      ["비디오 수", numberOfVideo],
+      ["이모티콘 수", numberOfEmoji],
+      ["유튜브 링크 수", numberOfYoutube]
+    ];
+
+    arr.forEach((element) => {
+      const divider = document.createElement("div");
+      const title = document.createElement("h4");
+      const content = document.createElement("h5");
+      title.textContent = element[0];
+      content.textContent = element[1];
+      divider.append(title);
+      divider.append(content);
+      parentNode.append(divider);
+    });
+  };
+
+  const displayPeriod = function (beginDate, endDate) {
+    const node = document.querySelector("#period");
+    const convertDate = function (date) {
+      const year = String(date.getFullYear()) + "년";
+      const month = String(date.getMonth() + 1) + "월";
+      const day = String(date.getDate()) + "일";
+
+      return year + " " + month + " " + day;
     };
 
-    node.textContent = convertDate(beginDate) + ' ~ ' + convertDate(endDate);
-  }
+    node.textContent = convertDate(beginDate) + " ~ " + convertDate(endDate);
+  };
 
-  const drawTextCloud = function (wordFrequency) {
-    const NUMBER_OF_WORDS = 250;
-
-    const parent = document.querySelector("#text-cloud").parentNode;
-    const wordsForCloud = [];
-    const trimmedWords = wordFrequency.slice(0, NUMBER_OF_WORDS);
-    const wordsMap = trimmedWords.map(function (element) {
-      return {text: element[0], size: element[1] * 5 };
-    });
-    const textCloud = wordCloud("#text-cloud", wordsMap, parent.offsetWidth, 500);
-    textCloud.update(wordsMap);
+  const displayTypingRanking = function (typingRanking) {
+    const chartNode = document.querySelector("#typing-chart");
+    const textNode = document.querySelector("#typing-ranking");
+    const top5 = typingRanking.slice(0, 5);
+    const chart = drawChart(chartNode, top5);
+    showRankText(textNode, typingRanking);
   };
 
   const displayNameArticle = function (nameFrequency) {
@@ -121,9 +172,9 @@ function readSingleFile(e) {
   const showRankText = function (domNode, list, cutline = -1) {
     let listSize = 0;
     const largestNum = list[0][1];
-    list.forEach(element => {
+    list.forEach((element) => {
       listSize += element[1];
-    })
+    });
 
     let topItems = list;
     if (cutline > 0) {
@@ -132,11 +183,11 @@ function readSingleFile(e) {
 
     topItems.forEach((element) => {
       const item = document.createElement("li");
-      const percent = Math.round(element[1]/listSize * 10000) / 100;
-      item.textContent = String(element[0]) + ' (' + String(percent) + '%)';
-      domNode.append(item); 
+      const percent = Math.round((element[1] / listSize) * 10000) / 100;
+      item.textContent = String(element[0]) + " (" + String(percent) + "%)";
+      domNode.append(item);
 
-      const width = String(element[1] * 100 / largestNum) + '%';
+      const width = String((element[1] * 100) / largestNum) + "%";
       item.style.width = width;
     });
   };
@@ -147,14 +198,5 @@ function readSingleFile(e) {
 }
 
 (function () {
-  const toggleFileContent = function () {
-    const btn = document.querySelector(".toggle-btn");
-    const fileContent = document.querySelector("#file-content");
-
-    btn.classList.toggle("active");
-    fileContent.classList.toggle("active");
-  };
-
   document.getElementById("file-input").addEventListener("change", readSingleFile, false);
-  document.querySelector(".toggle-btn").addEventListener("click", toggleFileContent);
 })();
--- js/script-obj.js
+++ js/script-obj.js
@@ -1,19 +1,63 @@
 import { generateHash } from "/js/md5.min.js";
 
 export const LangEnum = Object.freeze({ ko: 0, en: 1, jp: 2 });
-const KO_DATE_REGEX = /(?<=\n)\d{4}년\s\d{1,2}월\s\d{1,2}일\s((오전)|(오후))\s\d{1,2}:\d{1,2}(?=,)/g;
+
+const KO_DATE_REGEX = /(?<=\r?\n)\d{4}년\s\d{1,2}월\s\d{1,2}일\s((오전)|(오후))\s\d{1,2}:\d{1,2}(?=,)/g;
 const KO_NAME_REGEX = /(?<=(\d{4}년\s\d{1,2}월\s\d{1,2}일\s((오전)|(오후))\s\d{1,2}:\d{1,2},\s)).+?(?=\s:\s)/g;
-const KO_CHATTING_REGEX = /(?<=(\d{4}년\s\d{1,2}월\s\d{1,2}일\s((오전)|(오후))\s\d{1,2}:\d{1,2},\s.+\s:\s)).+/g
-const KO_REGEX_WORDS_FILTER = /(ㅋ+)|(ㅇ+)|(ㅎ+)|(사진)|(이모티콘)|(ㅇㅅㅇ)|(ㄹㄱㄴ)|(ㅗㅜㅑ)|(샵검색:)|(아니)|(저거)|(와)|(왜)|(진짜)|(이제)|(근데)|(난)|(내가)|(지금)|(이거)|(그리고)|(요즘)|(그냥)|(너무)/g
-const postposition = ['이', '가', '께서', '에서', '서', '이다', '의', '을', '를', '에', '에게', '께', '한테', '에서', '에게서', '로', '로서', '고', '라고', '이라고', '와', '과', '랑', '이랑', '처럼', '만큼', '요', '밖에', '이나', '이란', '이든가', '이든지', '이나마', '이야말로'];
+const KO_CHATTING_REGEX = /(?<=(\d{4}년\s\d{1,2}월\s\d{1,2}일\s((오전)|(오후))\s\d{1,2}:\d{1,2},\s.+\s:\s)).+/g;
+const KO_CHATTING_AND_NAME_REGEX = /(?<=(\d{4}년\s\d{1,2}월\s\d{1,2}일\s((오전)|(오후))\s\d{1,2}:\d{1,2},\s))[\s\S]+?(?=\r?\n\d{4}년)/g;
 
-export function createScriptObj(content){
+const KO_PHOTO_REGEX = /\s:\s사진\r?\n/g;
+const KO_VIDEO_REGEX = /\s:\s동영상\r?\n/g;
+const KO_EMOJI_REGEX = /\s:\s이모티콘\r?\n/g;
+const YOUTUBE_LINK_REGEX = /(https:\/\/youtu\.be\/.+)|(https:\/\/www\.youtube\.com\/.+)/g;
+
+export function createScriptObj(content) {
   //Private Method
-  const countWordsFrequency = (parsedWords) => {return countFrequency(parsedWords)};
+  const countMatchToRegex = function (str, regex) {
+    return ((str || "").match(regex) || []).length;
+  };
 
-  const countDayFrequency = function(parsedDate){
-    const dayArray = []
-    parsedDate.forEach(element => {
+  const getTypingRanking = function (script) {
+    const chattingArray = script.match(KO_CHATTING_AND_NAME_REGEX);
+    let rankingMap = new Map();
+
+    for (let i = 0; i < chattingArray.length; i++) {
+      if (
+        /This message has been hidden by the chatroom managers\./g.test(chattingArray[i]) ||
+        /님이 나갔습니다\./g.test(chattingArray[i]) ||
+        /방장이 .+?님에서 .+?님으로 변경되었습니다.\r?\n.+?님은 참여자 관리와 채팅방 관리에 신경써주세요!/g.test(chattingArray[i]) ||
+        /.+?이 들어왔습니다\./g.test(chattingArray[i])
+      ) {
+        continue;
+      }
+
+      try{
+        const nickname = chattingArray[i].match(/^(.+?)(?=\s:\s)/gm)[0];
+        const chatting = chattingArray[i].match(/(?<=.+?\s:\s)[\s\S]+/g)[0];
+        
+        if (rankingMap.has(nickname)) {
+          rankingMap.set(nickname, rankingMap.get(nickname) + chatting.length);
+        } else {
+          rankingMap.set(nickname, chatting.length);
+        }
+      }
+      catch (e) {
+        console.error(e);
+        continue;
+      }
+    }
+
+    const arrForSort = Array.from(rankingMap);
+    arrForSort.sort(function (first, second) {
+      return second[1] - first[1];
+    });
+    return arrForSort;
+  };
+
+  const countDayFrequency = function (parsedDate) {
+    const dayArray = [];
+    parsedDate.forEach((element) => {
       const dayLabel = getDayLabel(element.getDay());
       dayArray.push(dayLabel);
     });
@@ -21,126 +65,155 @@ export function createScriptObj(content){
     return countFrequency(dayArray);
   };
 
-  const countHourFreqeuncy = function(parsedDate){
-    const hourArray = []
-    parsedDate.forEach(element => {
+  const countHourFreqeuncy = function (parsedDate) {
+    const hourArray = [];
+    parsedDate.forEach((element) => {
       hourArray.push(element.getHours());
     });
 
     return countFrequency(hourArray);
   };
 
-  const countNameFrequnecy = function(script){
+  const countNameFrequnecy = function (script) {
     const nameArray = script.match(KO_NAME_REGEX);
     return countFrequency(nameArray);
-  }
+  };
 
-  const parseToDateArray = function(script){
+  const parseToDateArray = function (script) {
     const dateStrArray = script.match(KO_DATE_REGEX);
     const dateArray = [];
     const KO_YEAR_REGEX = /\d{4}(?=년)/;
     const KO_MONTH_REGEX = /\d{1,2}(?=월)/;
     const KO_DAY_REGEX = /\d{1,2}(?=일)/;
     const KO_TIME_REGEX = /(?<=((오전)|(오후))\s)\d{1,2}:\d{1,2}/;
-    
-    dateStrArray.forEach(element => {
+
+    dateStrArray.forEach((element) => {
       const year = parseInt(element.match(KO_YEAR_REGEX));
       const month = parseInt(element.match(KO_MONTH_REGEX));
       const day = parseInt(element.match(KO_DAY_REGEX));
-      const timeStrArray = element.match(KO_TIME_REGEX)[0].split(':');
+      const timeStrArray = element.match(KO_TIME_REGEX)[0].split(":");
       let hour = parseInt(timeStrArray[0]);
       const minute = parseInt(timeStrArray[1]);
-      const isAfternoon = new RegExp('(오후)').test(element);
+      const isAfternoon = new RegExp("(오후)").test(element);
 
-      if (isAfternoon){
+      if (isAfternoon) {
         hour += 12;
       }
 
       dateArray.push(new Date(year, month - 1, day, hour, minute));
     });
-    
+
     return dateArray;
   };
 
-  const parseWords = function(script){
-    let words =[]
-    const trimmed = script.replaceAll(KO_REGEX_WORDS_FILTER, ' ');
-    const chattingLines = trimmed.match(KO_CHATTING_REGEX);
-    chattingLines.forEach(element => {
-      words = words.concat(element.split(' '));
-    });
-    for(let i = 0; i < words.length; i++){
-      words[i] = removePostposition(words[i]);
-    }
-
-    return words;
-  };
-
-  const countFrequency = function(arr){
+  const countFrequency = function (arr) {
     let map = new Map();
-    for(let i = 0; i < arr.length; i++){
-      if(map.has(arr[i])){
+    for (let i = 0; i < arr.length; i++) {
+      if (map.has(arr[i])) {
         map.set(arr[i], map.get(arr[i]) + 1);
-      }
-      else{
+      } else {
         map.set(arr[i], 1);
       }
     }
 
     const arrForSort = Array.from(map);
-    arrForSort.sort(function (first, second) {return second[1] - first[1];});
+    arrForSort.sort(function (first, second) {
+      return second[1] - first[1];
+    });
     return arrForSort;
   };
 
-  const removePostposition = function(word) {
-    postposition.forEach(filterWord => {
-      if(word.endsWith(filterWord)){
-        return word.slice(0,word.length - filterWord.length);
+  const removePostposition = function (word) {
+    postposition.forEach((filterWord) => {
+      if (word.endsWith(filterWord)) {
+        return word.slice(0, word.length - filterWord.length);
       }
     });
     return word;
-  }
+  };
 
-  const getDayLabel = function(day) {
+  const getDayLabel = function (day) {
     const week = new Array("일요일", "월요일", "화요일", "수요일", "목요일", "금요일", "토요일");
     const dayLabel = week[day];
     return dayLabel;
-  }
+  };
 
   //Property
   const md5Hash = generateHash(content);
   const script = content;
   const parsedDates = parseToDateArray(script);
-  //const parsedWords = parseWords(script);
   //Support Korean only for now
-  const lang = /(?<=\n)(저장한 날짜)\s:\s\d{4}년\s\d{1,2}월\s\d{1,2}일\s((오전)|(오후))\s\d{1,2}:\d{1,2}(?=\n)/.test(script)?LangEnum.ko : null;
-  //const wordFrequency = countWordsFrequency(parsedWords);
+  const lang = /(?<=\r?\n)(저장한 날짜)\s:\s\d{4}년\s\d{1,2}월\s\d{1,2}일\s((오전)|(오후))\s\d{1,2}:\d{1,2}(?=\r?\n)/.test(
+    script
+  )
+    ? LangEnum.ko
+    : null;
   const nameFrequency = countNameFrequnecy(script);
   const dayFrequency = countDayFrequency(parsedDates);
   const hourFrequency = countHourFreqeuncy(parsedDates);
+
+  const numberOfPhoto = countMatchToRegex(script, KO_PHOTO_REGEX);
+  const numberOfVideo = countMatchToRegex(script, KO_VIDEO_REGEX);
+  const numberOfEmoji = countMatchToRegex(script, KO_EMOJI_REGEX);
+  const numberOfYoutube = countMatchToRegex(script, YOUTUBE_LINK_REGEX);
+
   const beginDate = parsedDates[0];
   const endDate = parsedDates[parsedDates.length - 1];
   const numberOfLines = parsedDates.length;
 
   return {
-    getMd5Hash: () => {return md5Hash;},
+    getMd5Hash: () => {
+      return md5Hash;
+    },
 
-    getLanguage: () => {return lang;},
+    getLanguage: () => {
+      return lang;
+    },
 
-    getScript: () => {return script;},
+    getScript: () => {
+      return script;
+    },
 
-    //getWordFrequency: () => {return wordFrequency;},
+    getNameFrequency: () => {
+      return nameFrequency;
+    },
 
-    getNameFrequency: () => {return nameFrequency;},
+    getDayFrequency: () => {
+      return dayFrequency;
+    },
 
-    getDayFrequency: () => {return dayFrequency;},
+    getHourFrequency: () => {
+      return hourFrequency;
+    },
 
-    getHourFrequency: () => {return hourFrequency},
+    getBeginDate: () => {
+      return beginDate;
+    },
 
-    getBeginDate: () => {return beginDate;},
+    getEndDate: () => {
+      return endDate;
+    },
 
-    getEndDate: () => {return endDate},
+    getPhotoFrequency: () => {
+      return numberOfPhoto;
+    },
 
-    getnumberOfLines: numberOfLines
+    getVideoFrequency: () => {
+      return numberOfVideo;
+    },
+
+    getEmojiFrequency: () => {
+      return numberOfEmoji;
+    },
+
+    getYoutubeFrequency: () => {
+      return numberOfYoutube;
+    },
+
+    getTypingRanking: () => {
+      return getTypingRanking(script);
+    },
+
+    numberOfLines: numberOfLines,
   };
 }
