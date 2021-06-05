b101995d5bd71af76475e3ab6fe0a7d03c888531
BK927 <dead4bees927@gmail.com>
2021-06-05 14:47:10 +0900

    feat: add expand button on CopingStyle

--- src/components/ChracterMaker/CopingStyle.js
+++ src/components/ChracterMaker/CopingStyle.js
@@ -1,6 +1,8 @@
-import React, { Fragment } from "react";
+import React from "react";
+import clsx from "clsx";
 import { makeStyles } from "@material-ui/core/styles";
 import PropTypes from "prop-types";
+import Collapse from "@material-ui/core/Collapse";
 import Typography from "@material-ui/core/Typography";
 import Table from "@material-ui/core/Table";
 import TableBody from "@material-ui/core/TableBody";
@@ -8,6 +10,8 @@ import TableCell from "@material-ui/core/TableCell";
 import TableContainer from "@material-ui/core/TableContainer";
 import TableRow from "@material-ui/core/TableRow";
 import SupervisorAccountIcon from "@material-ui/icons/SupervisorAccount";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import IconButton from "@material-ui/core/IconButton";
 
 const useStyles = makeStyles((theme) => ({
   subtitle: {
@@ -18,10 +22,25 @@ const useStyles = makeStyles((theme) => ({
     fontWeight: "bold",
     whiteSpace: "nowrap",
   },
+  expand: {
+    transform: "rotate(0deg)",
+    marginLeft: "auto",
+    transition: theme.transitions.create("transform", {
+      duration: theme.transitions.duration.shortest,
+    }),
+  },
+  expandOpen: {
+    transform: "rotate(180deg)",
+  },
 }));
 
 function CopingStyle({ count, name, description, behaviors, examples }) {
   const classes = useStyles();
+  const [expanded, setExpanded] = React.useState(false);
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
 
   function createRow(title, content) {
     return { title, content };
@@ -47,18 +66,30 @@ function CopingStyle({ count, name, description, behaviors, examples }) {
         <SupervisorAccountIcon />
         대처 방식{count} : {name}
       </Typography>
-      <Table aria-label="simple table">
-        <TableBody>
-          {rows.map((row, index) => (
-            <TableRow key={index}>
-              <TableCell className={classes.titleCell} align="left">
-                {row.title}
-              </TableCell>
-              <TableCell align="left">{row.content}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
+      <IconButton
+        className={clsx(classes.expand, {
+          [classes.expandOpen]: expanded,
+        })}
+        onClick={handleExpandClick}
+        aria-expanded={expanded}
+        aria-label="show more"
+      >
+        <ExpandMoreIcon />
+      </IconButton>
+      <Collapse in={expanded} timeout="auto" unmountOnExit>
+        <Table aria-label="simple table">
+          <TableBody>
+            {rows.map((row, index) => (
+              <TableRow key={index}>
+                <TableCell className={classes.titleCell} align="left">
+                  {row.title}
+                </TableCell>
+                <TableCell align="left">{row.content}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </Collapse>
     </TableContainer>
   );
 }
--- src/components/ChracterMaker/SchemaDetail.js
+++ src/components/ChracterMaker/SchemaDetail.js
@@ -1,5 +1,4 @@
 import React from "react";
-import Divider from "@material-ui/core/Divider";
 import { makeStyles } from "@material-ui/core/styles";
 import Table from "@material-ui/core/Table";
 import TableBody from "@material-ui/core/TableBody";
