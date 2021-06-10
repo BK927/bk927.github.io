403bb1b0b6d6c01c9b3e89792f94adc38256cbae
BK927 <dead4bees927@gmail.com>
2021-06-10 17:08:17 +0900

    feat: add conditional shema explanation

--- src/components/ChracterMaker/SchemaItem.js
+++ src/components/ChracterMaker/SchemaItem.js
@@ -94,16 +94,18 @@ function SchemaItem({ schemaObj, conditionalFlag }) {
   const modalTitle = "무조건 심리도식과 조건 심리도식";
   const modalContent = (
     <Fragment>
-      <Typography gutterBottom variant="body1">
-        심리도식(Schema)이란 개인이 세상을 바라보는 틀입니다. 사람은 심리도식을 통해 '이러이러한 상황에서는 이렇게
-        생각하고, 저렇게 느끼며, 어떻게 행동해야 한다'을 결정합니다. 심리도식은 사람에 따라 의식 할 수도 있지만, 그런
-        심리도식을 가지고 있다는 것을 깨닫지 못할 수도 있습니다.
+      <Typography gutterBottom variant="h5">
+        무조건적 심리도식
       </Typography>
       <Typography gutterBottom variant="body1">
-        시나리오를 작성하는 데 있어서 심리도식은 캐릭터에게 입체성을 부여해 줄 수 있습니다. 캐릭터는 과거의 트라우마으로
-        인해서 심리도식을 가집니다. 그리고 스토리가 진행됨에 따라 심리도식을 맹목적으로 혹은 고집스럽게 따르다가 갈등을
-        겪습니다. 때로는 갈등을 통해 심리도식을 깨닫고 성장을 이룰 수도 있습니다. 때로는 더 심리도식에 매달리면서 깊은
-        나락을 떨어질 수도 있습니다. 그것도 아니라면 새로운 심리도식을 얻을 수도 있습니다.
+        무조건적 심리도식은 말 그대로 타인이나 자신에 대한 무조건적 믿음입니다.  무조건적 심리도식은 자신이 무언가를 해서 나아질 수 있다는 희망을 가지지 않습니다. 무조건적 심리도식을 가지고 있는 사람은 그 심리도식에 의해 사랑받을 만하지 않고, 잘 적응하지 못하고, 무능하고, 위험에 처해있다 고 믿습니다. 아니 믿는다는 사실도 자각하지 못하고 그냥 그들에게 심리도식은 진실이자 사실입니다. 사슬 아이콘 왼쪽 혹은 위에 표시되고 있는 심리도식이 무조건 심리도식입니다.
+      </Typography>
+      <p></p>
+      <Typography gutterBottom variant="h5">
+        조건적 심리도식
+      </Typography>
+      <Typography gutterBottom variant="body1">
+        조건적 심리도식은 무조건적 심리도식으로 부터 생기는 고통을 경감시키기 위해 생겨납니다. 그래서 조건적 심리도식은 자신이 무언가를 하면 나아질 수 있다는 희망을 가지게 합니다. 예를들어  <Box component="span" fontWeight='fontWeightBold' fontStyle="italic">결함</Box> 도식에 대해 <Box component="span" fontWeight='fontWeightBold' fontStyle="italic">자기희생</Box> 도식을 발달시키면 <Box component="span" fontWeight='fontWeightBold' fontStyle="italic">"내가 내 욕구를 무시하면서까지 너의 모든 욕구를 충족시켜주면, 너는 내가 결함이 있어도 받아줄거야. 그러면 나는 사랑받을 수 없다고 느끼지 않게 될거야"</Box>라고 생각하게 됩니다. 그러나 그런 믿음과 달리 조건적 도식으로는 무조건적 도식을 충족시킬 수 없고 마음의 안정도 도식을 직시하기 전까지 영원히 찾을 수 없습니다. 사슬 아이콘 오른쪽 혹은 아래 표시되고 있는 심리도식이 조건 심리도식입니다.
       </Typography>
     </Fragment>
   );
