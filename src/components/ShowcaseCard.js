30054c327e38800b9fd832d6dac3ab5d266483c2
BK927 <dead4bees927@gmail.com>
2021-06-14 23:33:55 +0900

    fix: fix ShowcaseCard alignment

--- src/components/ShowcaseCard.js
+++ src/components/ShowcaseCard.js
@@ -3,22 +3,28 @@ import Card from "@material-ui/core/Card";
 import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
 import { Link } from "react-router-dom";
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    "&&":{
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "space-between",
+    "&&": {
       backgroundColor: theme.palette.background.dp02,
     },
   },
-  body:{
-    marginTop:12,
-  }
+  body: {
+    marginTop: 12,
+  },
+  action: {
+    marginBottom: theme.spacing(1.5),
+  },
 }));
 
-function ShowcaseCard({title, category, body, to, action}) {
+function ShowcaseCard({ title, category, body, to, action }) {
   const classes = useStyles();
 
   return (
@@ -27,14 +33,12 @@ function ShowcaseCard({title, category, body, to, action}) {
         <Typography variant="h5" component="h2">
           {title}
         </Typography>
-        <Typography  color="textSecondary">
-          {category}
-        </Typography>
+        <Typography color="textSecondary">{category}</Typography>
         <Typography className={classes.body} variant="body2" component="p">
           {body}
         </Typography>
       </CardContent>
-      <CardActions>
+      <CardActions className={classes.action}>
         <Button component={Link} to={to} size="small">
           {action}
         </Button>
@@ -51,7 +55,7 @@ ShowcaseCard.propTypes = {
 };
 
 ShowcaseCard.defaultProps = {
-  action: '이동하기',
+  action: "이동하기",
 };
 
 export default ShowcaseCard;
